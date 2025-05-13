e0e3e8e2fbea2cf818652a83ddbdcea46dc660f0
cisdeli <pedrohcisdeli@gmail.com>
2025-05-13 16:13:34 -0400

    chore: testing new config for a single xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,6 @@
 module.exports = {
   siteUrl: "https://ciampitti-lab.github.io",
   generateRobotsTxt: true, // creates robots.txt with a Sitemap: line
+  generateIndexSitemap: false, // creates a separate index sitemap file
   trailingSlash: false,
 };
